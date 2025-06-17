28083889657dc14dc37301eb22cda365037b9cf7
Ahmad Luthfi <ahmadluthfi@deptechdigital.com>
2025-06-17 13:27:42 +0700

    login page done slicing

--- app/src/main/java/com/aluth/storyapp/view/LoginActivity.kt
+++ app/src/main/java/com/aluth/storyapp/view/LoginActivity.kt
@@ -1,21 +1,31 @@
 package com.aluth.storyapp.view
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.aluth.storyapp.R
+import com.aluth.storyapp.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_login)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        binding.tvSignup.setOnClickListener {
+            Log.d("Login", "Bisa meureun")
+            Toast.makeText(this, "Uhuyy", Toast.LENGTH_SHORT).show()
         }
     }
 }
